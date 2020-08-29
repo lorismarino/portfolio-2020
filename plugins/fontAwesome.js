04a6a47631da7adc2b1e4677f3f6c44ea9f120fe
Loris Marino <loris.mro@gmail.com>
2020-08-29 22:23:12 +0200

    feat(header): create header

--- plugins/fontAwesome.js
+++ plugins/fontAwesome.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLongArrowAltRight,
+  faEnvelopeSquare,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faLongArrowAltRight)
+library.add(faLongArrowAltRight, faEnvelopeSquare, faGithubSquare, faLinkedin)
 
 Vue.component('fa', FontAwesomeIcon)
